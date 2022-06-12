dbcb6440c37d9084194d805deed4b61eac36e29e
Vigneshwaran E <vigneshwarane@botvfx.com>
2022-06-12 20:18:35 +0530

    cart item completed

--- src/componets/cart-dropdown/cart-dropdown.component.jsx
+++ src/componets/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { CartContext } from '../../contexts/cart.context';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-  <div className='cart-dropdown-container'>
-    <div className='cart-items'>
-      {/* [].map((item) => (<CartItem cartItem={item} />
-      )) */}
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>Checkout</Button>
     </div>
-    <Button>Checkout</Button>
-  </div>
-);
+  );
+};
 
 export default CartDropdown;
