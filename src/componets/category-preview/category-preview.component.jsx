b99677e3cd8074782b5d49abf0134ce54c9ee1d6
Vigneshwaran E <vigneshwarane@botvfx.com>
2022-06-14 17:49:12 +0530

    styling complete

---
+++ src/componets/category-preview/category-preview.component.jsx
@@ -0,0 +1,24 @@
+import './category-preview.styles.scss';
+import { Link } from 'react-router-dom';
+import ProductCard from '../product-card/product-card.componet';
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className='category-preview-container'>
+      <h2>
+        <Link className='title' to={title}>
+          {title.toUpperCase()}
+        </Link>
+      </h2>
+      <div className='preview'>
+        {products
+          .filter((product, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPreview;
